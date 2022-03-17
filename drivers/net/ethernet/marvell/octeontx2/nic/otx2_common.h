140edb4460d42f0ed9fb8a1aceccba8a59dd5570
Subbaraya Sundeep <sbhatta@marvell.com>
2022-03-17 15:03:31 +0530

    Revert "octeontx2-pf: Vary completion queue event size"
    Until ethtool utility supports changing CQE use devlink
    interface only. Hence reverting the patch.
    
    Change-Id: Icf7d0f8122dc3c1192b5e6638356602385624c23
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/72996
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -182,7 +182,8 @@ struct otx2_hw {
 
 #define OTX2_DEFAULT_RBUF_LEN	2048
 	u16			rbuf_len;
-	u32			xqe_size;
+
+	u16			xqe_size;
 
 	/* NPA */
 	u32			stack_pg_ptrs;  /* No of ptrs per stack page */
